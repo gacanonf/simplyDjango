3052b53c300eb1c19e589746e0274dfc93a7374c
gcanon <gcanon@gmail.com>
2018-01-26 13:50:59 +0100

    Initial commit

---
+++ DjangoApp/urls.py
@@ -0,0 +1,39 @@
+"""
+Definition of urls for DjangoApp.
+"""
+
+from datetime import datetime
+from django.conf.urls import url
+from app.forms import BootstrapAuthenticationForm
+from app.views import *
+from app.models import *
+from django.contrib.auth.views import *
+
+
+# Uncomment the next lines to enable the admin:
+from django.conf.urls import include
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = [
+    # Examples:
+     
+    url(r'^$', home, name='home'),
+    url(r'^contact$', contact, name='contact'),
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^admin/', include(admin.site.urls)),
+
+
+    url(r'^about', about, name='about'),
+   url(r'^login/$', login, {
+            'template_name': 'app/login.html',
+            'authentication_form': BootstrapAuthenticationForm,
+            'extra_context':
+            {
+                'title':'Log in',
+                'year':datetime.now().year,
+            }
+        },
+        name='login'),
+       url(r'^logout$', logout, {  'next_page': '/'  },        name='logout')
+]
